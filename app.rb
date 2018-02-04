986f951ecc78344e00736afd0eec8cf29c876bc9
Brandon Leeds <brandonleeds@gmail.com>
2018-02-04 20:47:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ class App < Sinatra::Base
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @values = params.values
-    @sentence = @values.each_with_index.collect { |x,i| "#{i} "}
+    @sentence = @values.each_with_index.collect { |x,i| "#{x} "}
   end
 
   get '/:operation/:number1/:number2' do
