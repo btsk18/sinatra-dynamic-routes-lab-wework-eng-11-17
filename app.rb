addb50269d27729f3f075a161ac0a5cee0ac89d0
Brandon Leeds <brandonleeds@gmail.com>
2018-02-04 20:41:29 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,13 +22,7 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @num = params[:number].to_i
-    @phrase = params[:phrase]
-    @new_string = ""
-    @num.times {
-      @new_string = @new_string + @phrase + " "
-    }
-    @new_string
+    @values = params.values
   end
 
   get '/:operation/:number1/:number2' do
