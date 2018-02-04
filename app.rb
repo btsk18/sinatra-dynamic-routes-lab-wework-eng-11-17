271ca21e43e17cc74a22fb9ceac6dbe583e14d26
Brandon Leeds <brandonleeds@gmail.com>
2018-02-04 20:40:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,6 +21,16 @@ class App < Sinatra::Base
     @new_string
   end
 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @num = params[:number].to_i
+    @phrase = params[:phrase]
+    @new_string = ""
+    @num.times {
+      @new_string = @new_string + @phrase + " "
+    }
+    @new_string
+  end
+
   get '/:operation/:number1/:number2' do
     @operation = params[:operation].downcase
     @num_1 = params[:number1]
